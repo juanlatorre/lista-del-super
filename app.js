c2c765f9a512527b54bcd4ead2f24dfba6f284ba
Juan Latorre <juanlatorre@protonmail.com>
2017-06-08 19:39:48 -0400

    job done, forgot to do commits xd

--- app.js
+++ app.js
@@ -1,46 +1,83 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var http = require('http')
+var express = require('express')
+var router = express.Router()
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var methodOverride = require('method-override')
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var csrf = require('csurf')
+var errorHandler = require('errorhandler')
+var session = require('express-session')
+var mongoskin = require('mongoskin')
+var db = mongoskin.db('mongodb://admin:jack@ds111622.mlab.com:11622/lista-del-super', {safe:true})
 
-var index = require('./routes/index');
-var tasks = require('./routes/tasks');
+// Routes
+var routes = require('./routes')
+var tasks = require('./routes/tasks')
 
-var app = express();
+var app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use( function(req,res,next) {
+  req.db = {}
+  req.db.tasks = db.collection('tasks')
+  next()
+})
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// all environments
+app.locals.appname = 'Lista de Supermercado'
+app.locals.moment = require('moment')
 
-app.use('/', index);
-app.use('/tasks', tasks);
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+//app.use(favicon(path.join('public','favicon.ico')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride())
+app.use(cookieParser('98DAF1999EA31E7A7B8E78E269EFE'))
+app.use(session({
+  secret: '98DAF1999EA31E7A7B8E78E269EFE',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(csrf())
 
-// catch 404 and forward to error handler
+app.use(require('less-middleware')(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  res.locals._csrf = req.csrfToken()
+  return next()
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.param('task_id', function(req, res, next, taskId) {
+  req.db.tasks.findById(taskId, function(error, task){
+    if (error) return next(error)
+    if (!task) return next(new Error('Task is not found.'))
+    req.task = task
+    return next()
+  })
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/', tasks.list)
+app.get('/tasks', tasks.list)
+app.post('/tasks', tasks.markAllCompleted)
+app.post('/tasks', tasks.add)
+app.post('/tasks/:task_id', tasks.markCompleted);
+app.delete('/tasks/:task_id', tasks.del);
+app.get('/tasks/completed', tasks.completed);
 
-module.exports = app;
+app.all('*', function(req, res){
+  res.status(404).send();
+})
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+})
