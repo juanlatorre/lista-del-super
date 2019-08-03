f105e4c8e1ab3923e9d8e14ec1a760dd94e92dfd
Juan Latorre <juanlatorre@protonmail.com>
2019-08-03 15:15:02 -0400

    secret firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCo5HGaWuev-uwd8T-iUpNgm0gHoD3pZaI",
-  authDomain: "lista-del-supermercado.firebaseapp.com",
-  databaseURL: "https://lista-del-supermercado.firebaseio.com",
-  projectId: "lista-del-supermercado",
-  storageBucket: "",
-  messagingSenderId: "465217254262",
-  appId: "1:465217254262:web:2f4f7f34394b914f"
+  //firebase keys//
 };
 
 firebase.initializeApp(firebaseConfig);
